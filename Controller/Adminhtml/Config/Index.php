ca23e9a20bb63236c8670c772a3bfb9a74d2c1ba
DevLatBo <oskr.cbba2010@gmail.com>
2025-03-18 22:55:59 -0400

    [M] Starting development of section tracker

---
+++ Controller/Adminhtml/Config/Index.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Devlat\ConfigTracker\Controller\Adminhtml\Config;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\Page;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action implements HttpGetActionInterface
+{
+
+    const ADMIN_RESOURCE = 'Devlat_ConfigTracker::config_track_logs';
+
+    /**
+     * @var PageFactory
+     */
+    private PageFactory $pageFactory;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $pageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $pageFactory
+    )
+    {
+        parent::__construct($context);
+        $this->pageFactory = $pageFactory;
+    }
+
+    /**
+     * Set title as "Tracker".
+     * @return Page
+     */
+    public function execute(): Page
+    {
+        $resultPage = $this->pageFactory->create();
+        $resultPage->setActiveMenu('Devlat_ConfigTracker::config_track_logs');
+        $resultPage->getConfig()->getTitle()->prepend(__('Tracker'));
+
+        return $resultPage;
+    }
+}
