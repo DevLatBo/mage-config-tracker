60a95fad6dcdfc2aaa47a8841b2fc9110e4555fa
DevLatBo <oskr.cbba2010@gmail.com>
2025-04-06 22:15:57 -0400

    [M] Adjustment for Verified Column

---
+++ view/adminhtml/web/js/grid/columns/custom.js
@@ -0,0 +1,27 @@
+define([
+    'underscore',
+    'Magento_Ui/js/grid/columns/select'
+], function (_, Column) {
+    'use strict';
+
+    return Column.extend({
+       defaults: {
+           bodyTmpl: 'Devlat_ConfigTracker/ui/grid/cells/text'
+       },
+        getOrderStatusColor: function (row) {
+           if (row.verified == '0') {
+               return 'not-verified';
+           } else if (row.verified == '1') {
+               return 'verified';
+           }
+           return '#303030';
+        },
+        getLabel: function(row) {
+           if (row.verified == '0') {
+               return "No";
+           } else if (row.verified == '1') {
+               return "Yes";
+           }
+        }
+    });
+});
