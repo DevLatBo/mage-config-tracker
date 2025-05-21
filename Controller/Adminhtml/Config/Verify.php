f7bd7fc54dbb3bc2657fea9ab6980cf89d12ab69
DevLatBo <oskr.cbba2010@gmail.com>
2025-05-20 22:18:28 -0400

    [M] Fixing error with controller and xml adjustments

---
+++ Controller/Adminhtml/Config/Verify.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Devlat\ConfigTracker\Controller\Adminhtml\Config;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\Page;
+use Magento\Framework\View\Result\PageFactory;
+
+class Verify extends Action implements HttpGetActionInterface
+{
+
+    const ADMIN_RESOURCE = 'Devlat_ConfigTracker::config_track_log_verify';
+
+    private PageFactory $pageFactory;
+
+    public function __construct(
+        PageFactory $pageFactory,
+        Context $context
+    )
+    {
+        parent::__construct($context);
+        $this->pageFactory = $pageFactory;
+    }
+
+    /**
+     * @return Page
+     */
+    public function execute(): Page
+    {
+        $resultPage = $this->pageFactory->create();
+        $resultPage->setActiveMenu('Magento_Backend::system');
+        $resultPage->getConfig()->getTitle()->prepend(__('Verify & Update'));
+
+        return $resultPage;
+    }
+}
---
+++ Ui/Component/Listing/Column/Actions.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Devlat\ConfigTracker\Ui\Component\Listing\Column;
+
+use Magento\Framework\UrlInterface;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Ui\Component\Listing\Columns\Column;
+
+class Actions extends Column
+{
+    private UrlInterface $urlBuilder;
+
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        UrlInterface $urlBuilder,
+        array $components = [],
+        array $data = []
+    )
+    {
+        $this->urlBuilder = $urlBuilder;
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    public function prepareDataSource(array $dataSource): array
+    {
+        if(!isset($dataSource['data']['items'])) {
+            return $dataSource;
+        }
+
+        foreach($dataSource['data']['items'] as &$item) {
+            if(!isset($item['id'])) {
+                continue;
+            }
+
+            $item[$this->getData('name')] = [
+                'edit' => [
+                    'href' => $this->urlBuilder->getUrl('tracker/config/verify', [
+                            'id' => $item['id'],
+                        ],
+                    ),
+                    'label' => __('Verify & Update'),
+                ]
+            ];
+        }
+        return $dataSource;
+    }
+}
