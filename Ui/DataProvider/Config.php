224fb3ec7d076411b711a03eac59de17368e317b
DevLatBo <oskr.cbba2010@gmail.com>
2025-08-16 19:12:59 -0400

    [A] Adding list of verified by users

--- Ui/DataProvider/Config.php
+++ Ui/DataProvider/Config.php
@@ -5,6 +5,7 @@ namespace Devlat\Settings\Ui\DataProvider;
 use Magento\Ui\DataProvider\AbstractDataProvider;
 use Devlat\Settings\Model\ResourceModel\Tracker\Collection as TrackerCollection;
 use Devlat\Settings\Model\ResourceModel\Tracker\CollectionFactory as TrackerCollectionFactory;
+use Magento\User\Model\ResourceModel\User\CollectionFactory as UserCollectionFactory;
 
 class Config extends AbstractDataProvider
 {
@@ -15,14 +16,18 @@ class Config extends AbstractDataProvider
 
     /** @var TrackerCollection  */
     protected $collection;
+    /**
+     * @var UserCollectionFactory
+     */
+    private UserCollectionFactory $userCollectionFactory;
 
 
     /**
-     * Constructor.
-     * @param string $name
-     * @param string $primaryFieldName
-     * @param string $requestFieldName
+     * @param $name
+     * @param $primaryFieldName
+     * @param $requestFieldName
      * @param TrackerCollectionFactory $trackerCollectionFactory
+     * @param UserCollectionFactory $userCollectionFactory
      * @param array $meta
      * @param array $data
      */
@@ -31,11 +36,13 @@ class Config extends AbstractDataProvider
         $primaryFieldName,
         $requestFieldName,
         TrackerCollectionFactory $trackerCollectionFactory,
+        UserCollectionFactory $userCollectionFactory,
         array $meta = [],
         array $data = []
     )
     {
         $this->collection = $trackerCollectionFactory->create();
+        $this->userCollectionFactory = $userCollectionFactory;
         parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data);
     }
 
@@ -46,11 +53,24 @@ class Config extends AbstractDataProvider
     {
         if (!isset($this->loadedData)) {
             $this->loadedData = [];
-
             foreach ($this->collection->getItems() as $item) {
-                $this->loadedData[$item->getData('id')] = $item->getData();
+                $data = $item->getData();
+
+                // In this part the verified_by is populated with username data instead of ids.
+                $userIds = !is_null($data['verified_by']) ? json_decode($data['verified_by'], true) : [];
+                $usernames = [];
+                if (!empty($userIds)) {
+                   $adminUserCollection = $this->userCollectionFactory->create()
+                    ->addFieldToFilter('user_id', ['in' => $userIds]);
+
+                   $usernames = $adminUserCollection->getColumnValues('username');
+                }
+                $data['verified_by'] = $usernames;
+
+                $this->loadedData[$item->getData('id')] = $data;
             }
         }
+
         return $this->loadedData;
     }
 }
